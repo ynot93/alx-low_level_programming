d4f48c570dccf1b6a2ce74e1e818b6845cbfa419
Tony Mputhia <tonymmputhia@gmail.com>
2023-07-13 16:36:22 -0700

    Add output to stderr

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,6 +10,8 @@
 int main(void)
 {
 	/* sizeof calculates the size of variables */
+	fprintf(stderr, "[stderr]: [Anything]\n")
+
 	printf("Size of a char: %zu byte(s)\n", sizeof(char));
 	printf("Size of a int: %zu byte(s)\n", sizeof(int));
 	printf("Size of a long int: %zu byte(s)\n", sizeof(long int));

eca856b49feb62908b2c129d1fb27cc3295427b3
Tony Mputhia <tonymmputhia@gmail.com>
2023-07-13 16:20:07 -0700

    Add comments on sizeof file

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -9,6 +9,7 @@
  */
 int main(void)
 {
+	/* sizeof calculates the size of variables */
 	printf("Size of a char: %zu byte(s)\n", sizeof(char));
 	printf("Size of a int: %zu byte(s)\n", sizeof(int));
 	printf("Size of a long int: %zu byte(s)\n", sizeof(long int));

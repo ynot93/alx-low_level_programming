ac0a0b3dc7e9d4be2c00a9b61660664a2b1b55b9
Tony Mputhia <tonymmputhia@gmail.com>
2023-07-14 08:13:23 -0700

    Print alphabet with exceptions

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet in lowercase
+ * apart from letter q and e
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+	{
+		if (ch != 'q' && ch != 'e')
+		{
+			putchar(ch);
+			ch += 1;
+		}
+	}
+	putchar ('\n');
+
+	return (0);
+}

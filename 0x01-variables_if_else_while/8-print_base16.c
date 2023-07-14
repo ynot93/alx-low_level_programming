d3607a8e10f729c07779bcbc447f9f0018e77a8a
Tony Mputhia <tonymmputhia@gmail.com>
2023-07-14 10:33:32 -0700

    Print hexadecimals in lowercase

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Prints all the numbers of base 16 in lowercase
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	char ch = '0';
+
+	while (ch <= '9')
+	{
+		putchar(ch);
+		ch++;
+	}
+
+	ch = 'a';
+	while (ch <= f)
+	{
+		putchar(ch);
+		ch++;
+	}
+	putchar('\n');
+
+	return (0);
+}

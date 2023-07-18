6901fc0e43f39325ec9c38e1425da32a9a824364
Tony Mputhia <tonymmputhia@gmail.com>
2023-07-18 13:57:46 -0700

    Print lowercase alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Description - Prints the alphabet in lowercase
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	/**
+	 * print_alphabet - Prints a-z
+	 *
+	 * Description: Utilizes the _putchar function to print
+	 * the alphabet in lowercase
+	 */
+	void print_alphabet(void)
+	{
+		int ch;
+
+		for (ch = 'a'; ch <= 'z'; ch++)
+			_putchar(ch);
+		_putchar('\n');
+	}
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -2,5 +2,6 @@
 #define MAIN_H
 
 int _putchar(int i);
+void print_alphabet(void);
 
 #endif

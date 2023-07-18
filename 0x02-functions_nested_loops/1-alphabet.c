c0474e27561f6ac40defebe3e9b02a3ca8100737
Tony Mputhia <tonymmputhia@gmail.com>
2023-07-18 14:21:39 -0700

    Fix nested function error

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,27 +1,16 @@
 #include "main.h"
 
 /**
- * main - Entry point
+ * print_alphabet - Prints a-z
  *
- * Description - Prints the alphabet in lowercase
- *
- * Return: Always 0 (Success)
- */
-int main(void)
+ * Description: Utilizes the _putchar function to print
+ * the alphabet in lowercase
+*/
+void print_alphabet(void)
 {
-	/**
-	 * print_alphabet - Prints a-z
-	 *
-	 * Description: Utilizes the _putchar function to print
-	 * the alphabet in lowercase
-	 */
-	void print_alphabet(void)
-	{
-		int ch;
+	int ch;
 
-		for (ch = 'a'; ch <= 'z'; ch++)
-			_putchar(ch);
-		_putchar('\n');
-	}
-	return (0);
+	for (ch = 'a'; ch <= 'z'; ch++)
+		_putchar(ch);
+	_putchar('\n');
 }

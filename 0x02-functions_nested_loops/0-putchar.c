0093003ce0e6a0eaac3c4515d57c6f9537b9ba32
Tony Mputhia <tonymmputhia@gmail.com>
2023-07-18 10:23:52 -0700

    Print _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Description - prints the word '_putchar'
+ *
+ * Return - Always 0 (Success)
+ */
+int main(void)
+{
+	print_putchar('_');
+	print_putchar('p');
+	print_putchar('u');
+	print_putchar('t');
+	print_putchar('c');
+	print_putchar('h');
+	print_putchar('a');
+	print_putchar('r');
+	print_putchar('\n');
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,6 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int print_putchar(int i);
+
+#endif

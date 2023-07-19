62abfbc62e2cd7cd32cad90b4ded007cfc6c9bf0
Tony Mputhia <tonymmputhia@gmail.com>
2023-07-18 17:07:52 -0700

    Return absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * int _abs - Computes the absolute value of an integer
+ * @n: The number to be checked
+ *
+ * Description: This function prints the sign of a given number. If the number
+ *              is either positive or negative, it returns the positive
+ *              value of it
+ * Return: A positive number whether input is positive or negative
+ */
+int_abs(int n)
+{
+	if (n > 0)
+		n *= -1
+		return (n);
+	return (n);
+}

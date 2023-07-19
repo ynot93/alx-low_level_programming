0cb16ca473221d178f8bda1e31f47bad2b42d7d2
Tony Mputhia <tonymmputhia@gmail.com>
2023-07-19 01:49:24 -0700

    Replace deleted file

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+/**
+ * add - Adds two integers
+ * @a: The first integer
+ * @b: The second integer
+ *
+ * Description: This function takes two integers and adds them together.
+ *              It returns the sum of the two integers.
+ *
+ * Return: The sum of the two integers
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}

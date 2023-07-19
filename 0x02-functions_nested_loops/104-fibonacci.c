06cb2b22273a17c4472c4a10d74b01ceb36223b3
Tony Mputhia <tonymmputhia@gmail.com>
2023-07-19 13:40:19 -0700

    First 98 fibonacci numbers

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+ * main - Prints the first 98 Fibonacci numbers, starting with 1 and 2
+ *
+ * Description: This program calculates the first 98 Fibonacci numbers
+ * without using long long, pointers, arrays, structures, or hardcoding
+ * any Fibonacci number (except for 1 and 2). The numbers are separated
+ * by comma followed by a space, and printed on a single line.
+ *
+ * Return: 0 on success
+ */
+int main(void)
+{
+	unsigned long int a = 1;
+	unsigned long int b = 2;
+	unsigned long int next;
+	int i;
+
+	printf("%lu, %lu", a, b);
+
+	for (i = 3, i <= 98, i++)
+	{
+		next = a + b;
+		printf(", %lu", next);
+		a = b;
+		b = next;
+	}
+	printf("\n");
+	return (0);
+}

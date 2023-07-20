3cf198f5445aafa8cd2b769d059afd10af628388
Tony Mputhia <tonymmputhia@gmail.com>
2023-07-20 08:51:26 -0700

    Fix missing brace

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,19 +2,22 @@
 
 /**
  * _isupper - Checks if a character is uppercase
- * @c - Character to be checked
+ * @c: Character to be checked
  *
  * Description - This function checks whether the input character 'c' is an
  * uppercase letter or not.
  *
  * Return: 1 if 'c' is uppercase, 0 otherwise
- *
  */
 
 int _isupper(int c)
 {
 	if (c >= 65 && c <= 90)
+	{
 		return (1);
+	}
 	else
+	{
 		return (0);
+	}
 }

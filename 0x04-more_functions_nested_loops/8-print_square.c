aa5c5e58329c5b607fc05aa526886a3853031ae3
Tony Mputhia <tonymmputhia@gmail.com>
2023-07-20 16:55:50 -0700

    Print a square

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_square - Print a square of a given size
+ * @size: The size of the square
+ */
+
+void print_square(int size)
+{
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+
+	int row, col;
+
+	for (row = 0; row < size; row++)
+	{
+		for (col = 0; col < size; col++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+}

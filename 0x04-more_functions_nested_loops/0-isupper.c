142ef96dde3a11316887354e48125ae0cfb261a6
Tony Mputhia <tonymmputhia@gmail.com>
2023-07-20 08:46:54 -0700

    Fix trailing whitespace

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -7,14 +7,14 @@
  * Description - This function checks whether the input character 'c' is an
  * uppercase letter or not.
  *
- * Return: 1 if 'c' is uppercase, 0 otherwise 
+ * Return: 1 if 'c' is uppercase, 0 otherwise
  *
  */
 
 int _isupper(int c)
 {
 	if (c >= 65 && c <= 90)
-		return 1;
+		return (1);
 	else
-		return 0;
+		return (0);
 }

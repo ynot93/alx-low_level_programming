17d20c36df2430e24b9a00d2321d8681f19cd445
Tony Mputhia <tonymmputhia@gmail.com>
2023-07-25 14:13:29 -0700

    Generate random passwords

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main - generates random valid passwords for the program 101-crackme
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	int pass[100];
+	int i;
+	int sum;
+	int n;
+
+	sum = 0;
+
+	for (i = 0; i < 100; 1++)
+	{
+		pass[i] = rand() % 78;
+		sum += (pass[i] + '0');
+		putchar(pass[i] + '0');
+		if ((2772 - sum) - '0' < 78)
+		{
+			n = 2772 - sum - '0';
+			sum += n;
+			putchar(n + '0');
+			break;
+		}
+	}
+
+	return (0);
+}

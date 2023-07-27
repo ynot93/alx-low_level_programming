d89162f12a7ecb80cbb7cf16bd86a8dc882303d3
Tony Mputhia <tonymmputhia@gmail.com>
2023-07-27 02:58:57 -0700

    Fix logic error

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,7 +10,7 @@
 
 char *_strcat(char *dest, char *src)
 {
-	char *ptr = *dest;
+	char *ptr = dest;
 
 	while (*ptr != '\0')
 	{
@@ -24,7 +24,7 @@ char *_strcat(char *dest, char *src)
 		src++;
 	}
 
-	*dest = '\0';
+	*ptr = '\0';
 
 	return (dest);
 }

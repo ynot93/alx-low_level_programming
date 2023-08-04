a41aab0ca80f6ffba37bbe5e25d81913b6411ff1
Tony Mputhia <tonymmputhia@gmail.com>
2023-08-04 11:55:14 -0700

    Fix syntax error

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -11,16 +11,18 @@
 
 int main(int argc, char **argv)
 {
+	int num1, num2, prod;
+
 	if (argc != 3)
 	{
 		printf("Error\n");
 		return (1);
 	}
 
-	int num1 = atoi(argv[1]);
-	int num2 = atoi(argv[2]);
-	int prod = num1 * num2;
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
+	prod = num1 * num2;
 
-	printf("%d\n", prod)
+	printf("%d\n", prod);
 	return (0);
 }

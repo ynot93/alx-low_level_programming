cd427d7739e2a12d7449e60588409c4b88fc93ce
Tony Mputhia <tonymmputhia@gmail.com>
2023-08-14 14:02:27 -0700

    define NULL as a constant

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,8 @@
 #ifndef DOG_H
 #define DOG_H
 
+#define NULL ((void *)0)
+
 /**
  * struct dog - Structure containing elements about a dog.
  * @name: Represents the name of the dog.
@@ -9,12 +11,13 @@
  *
  * Description: Describes a dog with the elements name, age and owner.
  */
-
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
 
 #endif

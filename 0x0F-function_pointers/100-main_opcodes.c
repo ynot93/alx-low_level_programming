fde67030091292efc8d8f0a4f5eb3f8772e821b3
Tony Mputhia <tonymmputhia@gmail.com>
2023-08-16 19:59:51 -0700

    OPcodes

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,37 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - Prints the operation codes of it's own main function.
+ * @argc: The number of arguments.
+ * @argv: An array of pointers to the arguments.
+ *
+ * Return: 0 (Success).
+ */
+int main(int argc, char *argv[])
+{
+	int number_of_bytes;
+	int i;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	number_of_bytes = atoi(argv[1]);
+
+	if (number_of_bytes < 0)
+	{
+		printf("Error\n");
+		return (2);
+	}
+
+	for (i = 0; i < number_of_bytes; i++)
+	{
+		printf("%02x")
+	}
+	printf("\n");
+
+	return (0);
+}

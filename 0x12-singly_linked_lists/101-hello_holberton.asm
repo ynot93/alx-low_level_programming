423c9e839928ae379a3ee461cd1135d543c080d0
Tony Mputhia <tonymmputhia@gmail.com>
2023-08-24 18:01:23 +0300

    Print Hello, Holberton in assembly language

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,17 @@
+section .data
+	hello db "Hello, Holberton", 0
+	format db "%s", 10, 0
+
+section .text
+	global main
+	extern printf
+
+main:
+	push rbp
+	mov rdi, format
+	mov rsi, hello
+	xor rax, rax
+	call printf
+	pop rbp
+
+	ret

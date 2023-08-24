4af9396467d14a2f55d5c2339c87f8ad7330b827
Tony Mputhia <tonymmputhia@gmail.com>
2023-08-24 15:53:39 +0300

    Using the constructor attribute

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "lists.h"
+/**
+ * print_message - Prints a message before main() is executed.
+ *
+ * Return: Nothing.
+ */
+void print_message(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}

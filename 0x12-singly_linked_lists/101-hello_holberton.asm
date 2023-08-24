7a5e1427b08f7cd1cb4719fc1f858f74199c88c3
Tony Mputhia <tonymmputhia@gmail.com>
2023-08-24 21:28:07 +0300

    Change syntax to match NASM assembler

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,17 +1,15 @@
-section .data
-	hello db "Hello, Holberton", 0
-	format db "%s", 10, 0
+	SECTION .data
+msg:	db "Hello, Holberton", 0
+fmt:	db "%s", 10, 0
 
-section .text
-	global main
+	SECTION .text
 	extern printf
-
+	global main
 main:
-	push rbp
-	mov rdi, format
-	mov rsi, hello
-	xor rax, rax
+	mov esi, msg
+	mov edi, fmt
+	mov eax, 0
 	call printf
-	pop rbp
 
+	mov eax, 0
 	ret

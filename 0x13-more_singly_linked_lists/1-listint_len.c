779c495fb2a667d844ed26ce9574e7910e2cd557
Tony Mputhia <tonymmputhia@gmail.com>
2023-08-28 09:15:01 -0700

    Handle empty list edge case

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * listint_len - Returns the number of elements in a linked list.
+ * @h: Pointer to move through the linked list.
+ *
+ * Return: The number of elements in the linked list.
+ */
+size_t listint_len(const listint_t *h)
+{
+	unsigned int count;
+
+	count = 0;
+
+

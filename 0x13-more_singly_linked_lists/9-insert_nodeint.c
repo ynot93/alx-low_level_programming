276b84f4861346432607c10f82a8732541fd5e2d
Tony Mputhia <tonymmputhia@gmail.com>
2023-08-31 00:09:35 +0300

    Fix logical error

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -16,28 +16,31 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	count = 0;
 	current = *head;
 
-	while (current != NULL && count < idx - 1)
-	{
-		current = current->next;
-		count++;
-	}
-
-	if (current == NULL && count < idx - 1)
-	{
-		return (NULL);
-	}
 	new_node = malloc(sizeof(listint_t));
 
 	if (new_node == NULL)
 		return (NULL);
 
 	new_node->n = n;
+	new_node->next = NULL;
 
 	if (idx == 0)
 	{
 		new_node->next = *head;
 		*head = new_node;
 		return (new_node);
+	}
+
+	while (current != NULL && count < idx - 1)
+	{
+		current = current->next;
+		count++;
+	}
+
+	if (current == NULL)
+	{
+		free(new_node);
+		return (NULL);
 	}
 	new_node->next = current->next;
 	current->next = new_node;

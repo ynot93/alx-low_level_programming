97b02142048d46b8629afdd55563fe983e1ecf43
Tony Mputhia <tonymmputhia@gmail.com>
2023-08-31 17:23:43 -0700

    Fix logical error

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -14,7 +14,7 @@ int clear_bit(unsigned long int *n, unsigned int index)
 	if (index >= (sizeof(unsigned long int) * 8))
 		return (-1);
 
-	mask = -(1UL << index);
+	mask = ~(1UL << index);
 
 	*n = *n & mask;
 

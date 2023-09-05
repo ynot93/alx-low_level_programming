afc95cd204ee9a8112a280f77d4bd22606ea1b61
Tony Mputhia <tonymmputhia@gmail.com>
2023-09-05 05:29:53 -0700

    Fix logical error

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,6 +11,9 @@ int create_file(const char *filename, char *text_content)
 {
 	int f_des, bytes_written, count;
 
+	ptr = text_content;
+	count = 0;
+
 	if (filename == NULL)
 		return (-1);
 	f_des = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0644);
@@ -21,7 +24,6 @@ int create_file(const char *filename, char *text_content)
 	while (*text_content)
 	{
 		count++;
-		text_content++;
 	}
 
 	if (text_content != NULL)

614bcf76c863347dae14eeaf936a2bd40650dde9
Tony Mputhia <tonymmputhia@gmail.com>
2023-09-05 06:10:29 -0700

    Fix counting characters syntax

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <string.h>
 /**
  * create_file - Creates a file.
  * @filename: Name of the file.
@@ -9,9 +10,8 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int f_des, bytes_written, count;
-
-	count = 0;
+	int f_des;
+	ssize_t bytes_written;
 
 	if (filename == NULL)
 		return (-1);
@@ -21,14 +21,9 @@ int create_file(const char *filename, char *text_content)
 	if (f_des == -1)
 		return (-1);
 
-	while (text_content[count])
-	{
-		count++;
-	}
-
 	if (text_content != NULL)
 	{
-		bytes_written = write(f_des, text_content, count);
+		bytes_written = write(f_des, text_content, strlen(text_content));
 
 		if (bytes_written == -1)
 		{

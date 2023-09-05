2d82ccc1aa0bcb46b2ad33c893b3430797d23396
Tony Mputhia <tonymmputhia@gmail.com>
2023-09-05 05:10:00 -0700

    Fix file permissions

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,7 +14,7 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 	
-	f_des = open(filename, O_WRONLY | O_CREAT, 0600);
+	f_des = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0644);
 
 	if (f_des == -1)
 		return (-1);

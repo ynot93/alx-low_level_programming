17fc0d7fbbe62bb272fa26d287d88921af79062a
Tony Mputhia <tonymmputhia@gmail.com>
2023-10-12 04:51:53 -0700

    Remove create_node function

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -10,8 +10,15 @@
  */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-	dlistint_t *new_node = create_node(n);
+	dlistint_t *new_node = malloc(sizeof(dlistint_t));
 
+	if (new_node == NULL)
+	{
+		perror("Malloc failed!");
+		return (NULL);
+	}
+	new_node->n = n;
+	new_node->prev = NULL;
 	new_node->next = *head;
 
 	if (*head != NULL)

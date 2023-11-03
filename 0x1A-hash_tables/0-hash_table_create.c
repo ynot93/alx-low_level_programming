124cbb0447d6d867aaf4caf24e14ec4cde6fadf1
Tony Mputhia <tonymmputhia@gmail.com>
2023-11-03 00:52:57 -0700

    Fix betty error

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -17,13 +17,13 @@ hash_table_t *hash_table_create(unsigned long int size)
 		return (NULL);
 
 	create_table = malloc(sizeof(hash_table_t));
-	
+
 	if (create_table == NULL)
 		return (NULL);
 
 	create_table->size = size;
 	create_table->array = malloc(sizeof(hash_node_t *) * size);
-	
+
 	if (create_table->array == NULL)
 	{
 		free(create_table);

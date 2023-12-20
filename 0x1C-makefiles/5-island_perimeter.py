b27cf338dc9ae2eaa70ce732e1072aeddd4b19d0
Tony Mputhia <tonymmputhia@gmail.com>
2023-12-20 13:20:11 -0800

    Island perimeter

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Deals with measuring the perimeter of an island.
+
+"""
+
+
+def island_perimeter(grid):
+    """
+    Calculates the island perimeter.
+
+    Returns:
+        int: perimeter of the island.
+
+    """

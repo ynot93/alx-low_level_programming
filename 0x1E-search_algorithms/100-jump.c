0159193143a240c392174e3436d25e005f6b60a4
Tony Mputhia <tonymmputhia@gmail.com>
2024-02-07 23:29:25 -0800

    Write space in printf text

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -31,7 +31,7 @@ int jump_search(int *array, size_t size, int value)
 	}
 
 	left = prev - jump;
-	printf("Value found between indexes[%lu] and [%lu]\n", left, prev);
+	printf("Value found between indexes [%lu] and [%lu]\n", left, prev);
 
 	for (i = left; i < (prev < size ? prev : size); ++i)
 	{
